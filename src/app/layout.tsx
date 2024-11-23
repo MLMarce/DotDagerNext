9f173ff9e312f4c6209438beacc947efd1fe6871
Marcelo Lencina <marcelodaniellencina@gmail.com>
2024-11-23 15:25:27 -0300

    fix bg color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,9 +26,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased scroll-hidden scroll-smooth transition`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased scroll-hidden scroll-smooth transitio`}
       >
         {children}
         <NavMenu />
